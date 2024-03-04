1c6b4533589da45ed7e290215d6cf32180e9065b
dylanlucko-hu <dylanlucko@gwu.edu>
2024-03-04 14:30:48 -0500

    change colour back to white

--- src/app.py
+++ src/app.py
@@ -89,7 +89,7 @@ import numpy as np
 app = dash.Dash(__name__)
 server = app.server
 
-app.layout = html.Div(style={'color': 'white'}, children=[
+app.layout = html.Div(style={'color': 'black'}, children=[
     html.H1("M/M/1 Queue Simulation", style={'text-align': 'center'}),
     html.Div(style={'display': 'flex'}, children=[
         html.Div(style={'flex': '50%', 'margin-right': '20px'}, children=[
